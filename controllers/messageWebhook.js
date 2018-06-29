4a5ca99d57b9b487be28592124ac0f0a482b48df
flickz <omoyajowo2015@gmail.com>
2018-06-29 15:41:41 +0100

    Add logger

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -2,11 +2,9 @@ const processMessage = require('../helpers/processMessage');
 
 module.exports = (req, res) => {
   if (req.body.object === 'page') {
-    console.log(req.body)
     req.body.entry.forEach(entry => {
       entry.messaging.forEach(event => {
         if (event.message && event.message.text) {
-          console.log(event.message, event.message.text)
           processMessage(event)
         }
       })
