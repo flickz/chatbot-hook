f3bbd392d238007f442863c575c34bc622c12912
flickz <omoyajowo2015@gmail.com>
2018-07-04 08:54:07 +0100

    Update messenger

--- controllers/intentWebhook.js
+++ controllers/intentWebhook.js
@@ -1,6 +1,6 @@
-const request = require('request');
+const { getUserProfile, sendPersistentMenu, sendPhoneNumberQuickReply, sendWelcomeText, sendQuickMenuReplies, sendJustText } = require('../lib/messenger')
 const fullFillmentTexts = require('./data/fullfillment_text');
-const { WebhookClient } = require('dialogflow-fulfillment');
+const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
 
 module.exports = (req, res) => {
   const agent = new WebhookClient({
@@ -9,10 +9,47 @@ module.exports = (req, res) => {
   })
   const data = fullFillmentTexts
   const intent = agent.intent;
-  const action = agent.action.toLocaleUpperCase();
-  console.log(action)
-  const parameters = agent.parameters;
-  console.log(`INFO: User asked, ${intent} for ${JSON.stringify(parameters)}, ${action}`)
-  agent.addResponse_(data['CHECK_CREDIT_BALANCE'].success)
-  agent.send_()
+  const action = agent.action;
+  const payload = agent.originalRequest.payload
+  const userPSID = payload.data.sender.id
+  
+  const handleUserProfile = function (error, profile) {
+    if(error) console.log(error);
+    const firstName = profile.first_name
+    sendJustText(userPSID, profile, (error, result) => {
+      if(error) {
+        return
+      }
+      console.log('Sent Welcome text')
+      sendQuickMenuReplies(userPSID, (error, result) => {
+        if(error) {
+          return
+        }
+        console.log('Quick reply')
+        agent.send_()
+      })
+    })
+  }
+  const handleCallbacks = function (error, result) {
+    if(error) {
+      return
+    }
+    getUserProfile(userPSID, handleUserProfile)
+  }
+  if (action === 'get_started') {
+    sendPersistentMenu(handleCallbacks)
+  }
+
+  if (action === 'check_balance') {
+    // agent.add('I will need to your phone number in order to check your balance, please enter your phone number')
+    sendPhoneNumberQuickReply(userPSID, (error, result) => {
+      if (error) {
+        console.log(error)
+        agent.send_()
+        return
+      }
+      console.log(result)
+      agent.send_()
+    })
+  }
 }
